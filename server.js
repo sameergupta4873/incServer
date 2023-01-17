bcfd3cc0dcb179fb30ee11d51560337b71c98ee6
Sameer Gupta <sameergupta4873@gmail.com>
2023-01-18 02:06:26 +0530

    code added

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
+
+dotenv.config({path:"config/config.env"})
+
+
+connectDatabase();
+
+app.listen(4000,()=>{
+  console.log("Server working on http://localhost:",process.env.PORT)
+})
